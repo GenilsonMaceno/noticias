fbfdcb6a5bd7fc89633e3a863be6c4b8528123a5
Genilson De Maceno <gmaceno@4bio.com.br>
2022-03-14 21:07:11 -0300

    first commit

---
+++ myapp/src/WEB/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace plataforma.noticias.web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
