eb387527f11b9685ef53f24303eae3ebbb572abf
Genilson De Maceno <gmaceno@4bio.com.br>
2022-03-14 23:03:07 -0300

    alter framework the net6.0 to net5.0

--- myapp/src/WEB/Models/ErrorViewModel.cs
+++ myapp/src/WEB/Models/ErrorViewModel.cs
@@ -1,8 +1,11 @@
-namespace plataforma.noticias.web.Models;
+using System;
 
-public class ErrorViewModel
+namespace plataforma.noticias.web.Models
 {
-    public string? RequestId { get; set; }
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
 
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
 }
